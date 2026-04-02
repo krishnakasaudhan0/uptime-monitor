9a2dd1400934f53a6b4b53745084f94ca27c0bfb
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-04-02 14:26:23 +0000

    fix: handle request timeouts gracefully

--- network/request.go
+++ network/request.go
@@ -4,3 +4,5 @@
 // Git contribution logic update at: 2026-04-02T04:32:05.000Z [hash: aiqc7]
 
 // Git contribution logic update at: 2026-04-02T08:22:59.000Z [hash: i22k2h]
+
+// Git contribution logic update at: 2026-04-02T14:26:23.000Z [hash: hfq1w]
